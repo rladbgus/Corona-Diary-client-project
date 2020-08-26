7cad2c4fa5bd3a8031c9234cd3277a9b746d0bdf
Myunggyun <sonmg90@gmail.com>
2020-08-26 21:23:37 +0900

    :sparkles: [추가] 페이지생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./page/Home";
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+      </Switch>
     </div>
   );
 }
