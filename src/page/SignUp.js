c96b24a9cc99e6678fdfe27640f7375812bdc492
Myunggyun <sonmg90@gmail.com>
2020-08-27 14:30:53 +0900

    :sparkles: [추가] Signup page - signup 페이지 추가

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Nav from "../component/Nav/Nav";
+import Signupform from "../component/Signup/SignUpForm";
+import styled from "styled-components";
+
+const Shift = styled.div`
+  margin-top: 50px;
+`;
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <Fragment>
+      <Nav />
+      <Shift />
+      <Signupform />
+    </Fragment>
+  );
 }
 
 export default SignUp;
