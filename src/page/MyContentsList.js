00b051c05c463f2a26d3fd1a9f57f1cb58852997
gunwooko <kkw722@gmail.com>
2020-08-28 11:21:45 +0900

    :sparkles: [추가] MyContentsList 뷰 추가

--- src/page/MyContentsList.js
+++ src/page/MyContentsList.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import Nav from "../component/Nav/Nav";
+import MyContentsListView from "../component/MyContentsList/MyContentsList";
 
-function MyContentsList() {
-  return <div>MyContentsList</div>;
-}
+const Shift = styled.div`
+  margin-top: 150px;
+`;
+
+const MyContentsList = () => {
+  return (
+    <>
+      <Nav />
+      <Shift />
+      <MyContentsListView />
+    </>
+  );
+};
 
 export default MyContentsList;
