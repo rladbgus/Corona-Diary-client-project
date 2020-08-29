9a2dd2e80b562caaf2f2c166241e8ff30493f415
Myunggyun <sonmg90@gmail.com>
2020-08-29 14:53:58 +0900

    :sparkles: [추가] signup 기능추가 -signup 중복확인 기 -signup 제출기능 -signup 취소버튼기능(추가적으로 수정필요)'

--- src/component/Signup/SignUpForm.js
+++ src/component/Signup/SignUpForm.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import SignUpFunction from "./SignUpFunction";
+import axios from "axios";
 
 const Container = styled.div`
   display: flex;
@@ -14,36 +16,7 @@ const Signupform = () => {
     <>
       <Container>
         <h1 title="signup">회원가입</h1>
-        <form>
-          <div>
-            <label>이메일</label>
-            <input className="input_email" type="email" />
-            <button className="email_check">중복확인</button>
-          </div>
-          <div>
-            <label>비밀번호</label>
-            <input className="input_password" type="password" />
-          </div>
-          <div>
-            <label>비밀번호확인</label>
-            <input className="input_password_check" type="password" />
-          </div>
-          <div>
-            <label>닉네임</label>
-            <input className="input_nickname" type="text" />
-            <button className="nickname_check">중복확인</button>
-          </div>
-          <div>
-            <label>나이대</label>
-            <input className="input_age" type="text" />
-          </div>
-          <div>
-            <label>격리된 지역</label>
-            <input className="input_location" type="text" />
-          </div>
-          <button type="submit">회원가입</button>
-          <button>취소</button>
-        </form>
+        <SignUpFunction />
       </Container>
     </>
   );
