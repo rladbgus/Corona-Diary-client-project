0ae8d6ea68a6c523301ab94989ece471e6e7a66d
rladbgus <cocokiuuu1858@gmail.com>
2020-08-29 16:08:50 +0900

    :sparkles: [추가] 햄버거버튼(로그인전), Nav바 기능구현  - 햄버거버튼 만들고 클릭시 redirecting  - 임시 로고 추가해놓기  - 로고클릭시 홈페이지로, 검색버튼클릭시 검색페이지로 (다른 페이지에서도 directing이 되도록 수정했음)

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { withRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import maakImg from "../../img/mask.jpg"
 
 const Header = styled.div`
   position: fixed;
   top: 0px;
   left: 0px;
   right: 0px;
-
   background-color: #cbe0e7;
   border: 0px;
   border-radius: 0, 0, 10px, 10px;
@@ -17,24 +17,55 @@ const Header = styled.div`
     float: right;
   }
 
-  .hamberger {
-    margin: 10px;
+  .menus {
+    margin: 12px;
   }
 
   .search_btn {
     margin: 10px;
   }
+
+  .open{
+  background-color: #F8EC7F;
+}
+
+.close{
+  display: none;
+}
+
+.logo {
+  width: 40px;
+  height: 40px;
+  margin: 5px 0px 0px 10px;
+}
 `;
 
-function Nav({ history }) {
+function Nav() {
+
+  const [MenuState, setIsMenuOpen] = useState(false);
+  console.log(MenuState);
+
   return (
     <Header>
-      <img className="logo" src="" alt="" />
-      <span className="home_name">Covid-19 Diary</span>
-      <button className="hamberger">햄버거</button>
-      <button className="search_btn" onClick={() => { history.push('./Search') }}>검색</button>
+      <img className="logo" src={maakImg} alt="logo" onClick={() => { window.location.href = 'http://localhost:3000' }} />
+
+      <button className="search_btn" onClick={() => { window.location.href = 'http://localhost:3000/Search' }}>검색</button>
+
+      <button className="menus" onClick={() => { setIsMenuOpen(!MenuState) }}>≡</button>
+      <ul className={MenuState ? "open" : "close"}>
+        <li>
+          <NavLink exact to="/user/login" activeClassName="selected">
+            로그인
+            </NavLink>
+        </li>
+        <li>
+          <NavLink to="/user/signup" activeClassName="selected">
+            회원가입
+            </NavLink>
+        </li>
+      </ul>
     </Header>
   );
 }
 
-export default withRouter(Nav);
+export default Nav;
