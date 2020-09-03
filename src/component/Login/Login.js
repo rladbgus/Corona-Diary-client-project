2ae105e2b4feabf4d3d5471bdda0b134150f7bd6
rladbgus <cocokiuuu1858@gmail.com>
2020-09-03 10:32:04 +0900

    :hammer: [수정] loginPage 불필요한 코드 삭제

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -32,28 +32,15 @@ const Login = (props) => {
                     value.handleToken(res.data.token);
                     props.history.push('/')
                 }
-                // else if (res.status === 409) {
-                //     alert('아이디,비밀번호를 확인해주세요')
-                // }
             })
             .catch(err => {
                 console.log(err)
                 alert('아이디,비밀번호를 확인해주세요')
             })
     }
-    // const onSignIn = (googleUser) => {
-    //     var profile = googleUser.getBasicProfile();
-    //     console.log('ID: ' + profile.getId());
-    //     console.log('Name: ' + profile.getName());
-    //     console.log('Image URL: ' + profile.getImageUrl());
-    //     console.log('Email: ' + profile.getEmail());
-    // }
 
     return (
         <center className="loginName" >
-            {/* <script src="https://apis.google.com/js/platform.js" async defer></script>
-            <meta name="google-signin-client_id" content="333133070398-amgnp101osuduqvjn2vacf3p20j2kmgn.apps.googleusercontent.com"></meta> */}
-
             <h1>로그인</h1>
             <form>
                 <div>
@@ -65,7 +52,6 @@ const Login = (props) => {
                     <input type="password" value={password} onChange={passwordHandler} />
                 </div>
                 <button onClick={submitHandler}>확인</button>
-                {/* <button className="g-signin2" onClick={onSignIn}>Google Login</button> */}
 
                 <div>
                     <GoogleLogin
