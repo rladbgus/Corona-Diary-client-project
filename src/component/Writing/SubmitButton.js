5a9fa06438d3148bf69736902f0c870cd5c4504a
gunwooko <kkw722@gmail.com>
2020-09-09 13:31:06 +0900

    :hammer: [수정] SubmitButton form-data 추가 - 패키지 추가: form-data - 수정내용: formData로 데이터 보내기, onClick으로 변경.

--- src/component/Writing/SubmitButton.js
+++ src/component/Writing/SubmitButton.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import axios from "axios";
 import getLogin from "../../Context/Context";
 import AlertModal from "../../Modal/AlertModal";
+const FormData = require("form-data");
 
 const Container = styled.div`
   display: flex;
@@ -20,7 +21,7 @@ const SubmitButton = ({ data, history }) => {
   const [children, getChildren] = useState("");
   const [className, getClassName] = useState("");
   const getToken = window.sessionStorage.getItem("token");
-  const [alldata, setAllData] = useState({ data: data });
+  // const [alldata, setAllData] = useState({ data: data });
 
   const openModal = () => {
     getModal(!modal);
@@ -30,14 +31,27 @@ const SubmitButton = ({ data, history }) => {
     getModal(!modal);
   };
 
-  const submitButton = event => {
+  const submitButton = (event) => {
     event.preventDefault();
-    // setAllData(alldata.data = data);
-    // if (Object.keys(data).length !== 11) {
-    //   getChildren("빈 항목이 있습니다. 채워주세요");
-    //   getClassName("checktdata");
-    //   return openModal();
-    // }
+    const formData = new FormData();
+    // formData.append("imgFile", "");
+    formData.append("title", data.title);
+    formData.append("text", data.text);
+    formData.append("covid_date", data.covid_date);
+    formData.append("q_temp", data.q_temp);
+    formData.append("q_resp", data.q_resp);
+    formData.append("q_cough", data.q_cough);
+    formData.append("q_appet", data.q_appet);
+    formData.append("q_sleep", data.q_sleep);
+    formData.append("q_fatigue", data.q_fatigue);
+    formData.append("q_psy", data.q_psy);
+    formData.append("tags", data.tags);
+
+    if (Object.keys(data).length !== 11) {
+      getChildren("빈 항목이 있습니다. 채워주세요");
+      getClassName("checktdata");
+      return openModal();
+    }
     if (data.title === "") {
       getChildren("제목을 채워주세요");
       getClassName("checktitle");
@@ -48,15 +62,16 @@ const SubmitButton = ({ data, history }) => {
       getClassName("checktext");
       return openModal();
     }
-    console.log(data);
+    console.log(formData.get("title"));
+    console.log(formData.get("text"));
+
     axios
-      .post(url, alldata, {
+      .post(url, formData, {
         headers: {
           "x-access-token": getToken,
-          "Content-Type": "mutipart/form-data"
         },
       })
-      .then(res => {
+      .then((res) => {
         console.log(res.data);
         history.push(`/content/${res.data.contentId}`);
       });
@@ -65,8 +80,10 @@ const SubmitButton = ({ data, history }) => {
   return (
     <>
       <Container>
-        <form onSubmit={submitButton}>
-          <button type="submit">일기등록</button>
+        <form>
+          <button onClick={submitButton} type="submit">
+            일기등록
+          </button>
           <Link to="/">
             <button>취소</button>
           </Link>
