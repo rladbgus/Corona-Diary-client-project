c1322afad9c652c18c02e5bedcbc5d312f19e909
Myunggyun <sonmg90@gmail.com>
2020-09-09 11:56:31 +0900

    :broken_heart: [버그] 로그인 관련 버그수정 작업내용 -닉네임 확인  창 오픈 -회원가입 후 로그인페이지이동

--- src/component/Content/Content.js
+++ src/component/Content/Content.js
@@ -10,19 +10,15 @@ import EditWritingPageForm from "./Edit/EditWritingPageForm";
 import Tags from "./Tags";
 import CheckingModal from "../../Modal/CheckingModal";
 import { useHistory } from "react-router-dom";
-
 const ContentBox = styled.div`
   background: #f0cdcd;
 `;
-
 const CommentBox = styled.div`
   background: #abe8e1;
 `;
-
 const CommentLi = styled.li`
   background: #f7ffaf;
 `;
-
 const LikeButton = styled.div`
   .likeBtn {
     background-color: #f0cdcd;
@@ -32,7 +28,6 @@ const LikeButton = styled.div`
     height: 30px;
   }
 `;
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -40,15 +35,11 @@ const Container = styled.div`
   align-items: center;
   margin: 10px;
 `;
-
 const ContentView = () => {
-
   let splitUrl = window.location.href.split("/");
   let contentId = splitUrl[4];
-
   const value = useContext(getLogin);
   let history = useHistory();
-
   const [content, setContent] = useState("");
   const [comment, newComment] = useState("");
   const [commented, setCommneted] = useState([]);
@@ -62,24 +53,19 @@ const ContentView = () => {
   const [countLike, setCountLike] = useState(0);
   const [tags, getTags] = useState([]);
   const [checkModal, getCheckModal] = useState(false);
-
   const openModalModify = () => {
     getCheckModal(!checkModal);
     getChildren("일기수정");
   };
-
   const closeCheckModal = () => {
     getCheckModal(!checkModal);
   };
-
   const openModal = () => {
     getModal(!modal);
   };
-
   const closeModal = () => {
     getModal(!modal);
   };
-
   useEffect(() => {
     const ac = new AbortController();
     axios
@@ -87,12 +73,13 @@ const ContentView = () => {
         headers: { "x-access-token": getToken },
       })
       .then(res => {
+        console.log(res.data.Content);
         setContent(res.data.Content);
         setnickName(res.data.Content.user.nickName);
         getTags(res.data.Content.tag);
         deleteButton();
         setCountLike(res.data.like);
-        value.setIsLike(res.data.Content.like[0].like)
+        value.setIsLike(res.data.Content.like[0].like);
         // userCommentBtn();
         // getCommentUser(res.data.contentDetail.comment.user.id);
       })
@@ -106,9 +93,7 @@ const ContentView = () => {
       ac.abort();
     };
   }, [commented, nickName]);
-
   const allComment = content.comment;
-
   const userInfo = () => {
     let ac = new AbortController();
     axios
@@ -124,7 +109,6 @@ const ContentView = () => {
       ac.abort();
     };
   };
-
   //좋아요버튼
   const setLikeBtn = () => {
     if (value.isLike) {
@@ -133,7 +117,6 @@ const ContentView = () => {
       plusLike();
     }
   };
-
   // 좋아요 증감
   const plusLike = () => {
     axios
@@ -144,10 +127,9 @@ const ContentView = () => {
       )
       .then(res => {
         setCountLike(res.data.count);
-        value.setIsLike(res.data.like)
+        value.setIsLike(res.data.like);
       });
   };
-
   //댓글기능
   const postComment = e => {
     e.preventDefault();
@@ -165,7 +147,6 @@ const ContentView = () => {
         newComment("");
       });
   };
-
   //댓글 삭제
   const deleteComment = value => {
     axios
@@ -184,21 +165,18 @@ const ContentView = () => {
         openModal();
       });
   };
-
   // 댓글 삭제,수정버튼 생성유무
   // const userCommentBtn = () => {
   //   if (value.nickName === commentUser) {
   //     setUserComment(!userComment)
   //   }
   // }
-
   //일기 삭제버튼 생성유무
   const deleteButton = () => {
     if (value.nickName === nickName) {
       getDelete(!deleteState);
     }
   };
-
   //일기 삭제
   const deleteContent = () => {
     axios
@@ -217,7 +195,6 @@ const ContentView = () => {
         return openModal();
       });
   };
-
   return (
     <center className="ContentViewBox">
       <>
@@ -232,27 +209,24 @@ const ContentView = () => {
                 <div>
                   <Tags data={tags} />
                 </div>
-
                 <LikeButton>
-                  {value.isLike ?
+                  {value.isLike ? (
                     <img
                       className="LikeImg"
                       src={heart}
                       alt=""
                       onClick={setLikeBtn}
                     />
-                    :
+                  ) : (
                     <img
                       className="LikeImg"
                       src={unheart}
                       alt=""
                       onClick={setLikeBtn}
-                    />}
-
-
-                  {countLike ? <span>{countLike}</span> : ''}
+                    />
+                  )}
+                  {countLike ? <span>{countLike}</span> : ""}
                 </LikeButton>
-
                 <button
                   onClick={openModalModify}
                   style={
@@ -264,7 +238,6 @@ const ContentView = () => {
                 <CheckingModal visible={checkModal} onClose={closeCheckModal}>
                   {children}
                 </CheckingModal>
-
                 <button
                   onClick={deleteContent}
                   style={
@@ -275,7 +248,6 @@ const ContentView = () => {
                 </button>
               </div>
             </ContentBox>
-
             <CommentBox>
               <div className="Comment">
                 <input
@@ -294,11 +266,9 @@ const ContentView = () => {
                       <br />
                       {data.comment}
                       <br />
-
                       <button onClick={() => deleteComment(data.id)}>
                         댓글 삭제
                       </button>
-
                       {/* style={
                         userComment ? { display: "none" } : { display: "block" }
                       } */}
@@ -309,10 +279,10 @@ const ContentView = () => {
             </CommentBox>
           </>
         ) : (
-            <Container>
-              <EditWritingPageForm content={content} token={getToken} />
-            </Container>
-          )}
+          <Container>
+            <EditWritingPageForm content={content} token={getToken} />
+          </Container>
+        )}
       </>
       <AlertModal visible={modal} onClose={closeModal} className={className}>
         {children}
@@ -320,5 +290,4 @@ const ContentView = () => {
     </center>
   );
 };
-
 export default ContentView;
