2a5cab1d0c501f181d4fe4cb00d496a2f6c400be
Myunggyun <sonmg90@gmail.com>
2020-09-16 16:28:19 +0900

    :nail_care: [CSS] 버튼글자색변경 작업내용 -로그인버튼 글자색 흰색으로 변경

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -24,21 +24,21 @@ const Login = () => {
     getModal(!modal);
   };
 
-  const emailHandler = (e) => {
+  const emailHandler = e => {
     setEmail(e.target.value);
   };
-  const passwordHandler = (e) => {
+  const passwordHandler = e => {
     setPassword(e.target.value);
   };
 
-  const submitHandler = (e) => {
+  const submitHandler = e => {
     e.preventDefault();
     axios
       .post(url + "/user/login", {
         email: email,
         password: password,
       })
-      .then((res) => {
+      .then(res => {
         if (res.status === 200) {
           value.handleSetNickName(res.data.nickName);
           getChildren("로그인에 성공했습니다");
@@ -55,12 +55,12 @@ const Login = () => {
       });
   };
 
-  const socialGoogleLogin = (googleToken) => {
+  const socialGoogleLogin = googleToken => {
     axios
       .post(url + "/user/socialLogin", {
         token: googleToken,
       })
-      .then((res) => {
+      .then(res => {
         if (res.status === 200) {
           value.handleLogin();
           value.handleToken(googleToken);
@@ -174,7 +174,7 @@ const LoginStyled = styled.div`
   }
   button {
     background: black;
-    color: #81c784;
+    color: #fff;
     border: none;
     display: inline-block;
     height: 3rem;
