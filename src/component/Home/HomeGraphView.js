a21f2c46b76e01be0e9992774c6ea82fa7464897
shinuhyun <shinuhyun@gmail.com>
2020-09-18 14:18:20 +0900

    :sparkles: [추가] HomeGraphView.js 추가

--- src/component/Home/HomeGraphView.js
+++ src/component/Home/HomeGraphView.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 const HomeGraphViewStyle = styled.div`
   background: #eec2f3;
@@ -18,11 +19,54 @@ const HomeGraphViewStyle = styled.div`
 `;
 
 const HomeGraphView = () => {
+  const url =
+    "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson";
+  const serviceKey =
+    "03zozd07ngOiK9lXcDXbElWQz7e%2FuNBN7Z5yGQeOlz%2FfT4wSjbxdeb9b80yMWLhCqKriZwLBrrD%2BzOqnD2hldQ%3D%3D";
+  const pageNo = 1;
+  const numOfRows = 10;
+  const startCreateDt = 20200310;
+  const endCreateDt = 20200315;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    let ac = new AbortController();
+    // axios.request({
+    //   method: "GET",
+    //   url: `${url}?$serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}&startCreateDt=${startCreateDt}&endCreateDt=${endCreateDt}`,
+    //   responseType: "arraybuffer",
+    //   responseEncoding: "binary",
+    // });
+    fetch(
+      `${url}?$serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}&startCreateDt=${startCreateDt}&endCreateDt=${endCreateDt}`,
+      {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "no-cors", // no-cors, cors, *same-origin)
+      }
+    )
+      // axios
+      //   .get(
+      //     `${url}?$serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}&startCreateDt=${startCreateDt}&endCreateDt=${endCreateDt}`,
+      //     { headers: { "Access-Control-Allow-Origin": "*" } }
+      //   )
+      .then((res) => {
+        console.log("Status", res.statusCode);
+        console.log("Headers", JSON.stringify(res.headers));
+        console.log("Reponse received", res.body);
+        console.log(res);
+        setData(res.body);
+      });
+    return () => {
+      ac.abort();
+    };
+  }, []);
+
   return (
     <HomeGraphViewStyle>
       <div className="homeGraphView">
         <div className="homeGraphContainer">
           <br />
+          {/* {data} */}
           <img></img>
         </div>
       </div>
